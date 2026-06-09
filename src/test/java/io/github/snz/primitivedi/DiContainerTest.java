f285e1cf53d57ed9bce14512dc6953244460b807
seanz89 <seanzfx@yandex.ru>
2026-06-09 14:55:59 +0300

    cover all explicit registration overloads

--- src/test/java/io/github/snz/primitivedi/DiContainerTest.java
+++ src/test/java/io/github/snz/primitivedi/DiContainerTest.java
@@ -1,16 +1,16 @@
 package io.github.snz.primitivedi;
 
-import io.github.snz.primitivedi.stub.ServiceA;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import io.github.snz.primitivedi.stub.*;
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
 
 class DiContainerTest {
     @Test
     void shouldRegisterAndResolveTransientDependency() {
-        DiContainer container = DiContainer.builder()
-                .transientOf(ServiceA.class)
-                .build();
+        DiContainer container =
+                DiContainer.builder().transientOf(ServiceA.class).build();
 
         ServiceA first = container.request(ServiceA.class);
         ServiceA second = container.request(ServiceA.class);
@@ -20,9 +20,7 @@ class DiContainerTest {
 
     @Test
     void shouldRegisterAndResolveScopedDependency() {
-        DiContainer container = DiContainer.builder()
-                .scopedOf(ServiceA.class)
-                .build();
+        DiContainer container = DiContainer.builder().scopedOf(ServiceA.class).build();
 
         try (DiContainerScope scope1 = container.createScope()) {
             ServiceA firstInScope1 = scope1.request(ServiceA.class);
@@ -40,9 +38,8 @@ class DiContainerTest {
 
     @Test
     void shouldRegisterAndResolveSingletonDependency() {
-        DiContainer container = DiContainer.builder()
-                .singletonOf(ServiceA.class)
-                .build();
+        DiContainer container =
+                DiContainer.builder().singletonOf(ServiceA.class).build();
 
         ServiceA first = container.request(ServiceA.class);
         ServiceA second = container.request(ServiceA.class);
@@ -53,12 +50,63 @@ class DiContainerTest {
     @Test
     void shouldRegisterAndResolvePreConstructedSingletonInstance() {
         ServiceA expected = new ServiceA();
-        DiContainer container = DiContainer.builder()
-                .singletonOf(ServiceA.class, expected)
-                .build();
+        DiContainer container =
+                DiContainer.builder().singletonOf(ServiceA.class, expected).build();
 
         ServiceA actual = container.request(ServiceA.class);
 
         assertSame(expected, actual);
+    }
+
+    @Test
+    void shouldResolveScopedDependencyAsSingletonWhenRequestedFromRootContainer() {
+        DiContainer container = DiContainer.builder().scopedOf(ServiceA.class).build();
+
+        ServiceA first = container.request(ServiceA.class);
+        ServiceA second = container.request(ServiceA.class);
+
+        assertSame(first, second);
+    }
+
+    @Test
+    void shouldRegisterUsingSingleParameterOverloads() {
+        DiContainer container = DiContainer.builder()
+                .transientOf(ServiceA.class)
+                .scopedOf(ServiceB.class)
+                .singletonOf(ServiceC.class)
+                .build();
+
+        container.request(ServiceA.class);
+        container.request(ServiceB.class);
+        container.request(ServiceC.class);
+    }
+
+    @Test
+    void shouldRegisterUsingFactoryOverloads() {
+        DiContainer container = DiContainer.builder()
+                .transientOf(ServiceA.class, c -> new ServiceA())
+                .scopedOf(ServiceB.class, c -> new ServiceB(c.request(ServiceA.class)))
+                .singletonOf(ServiceC.class, (Function<DiContainerBase, ServiceC>)
+                        c -> new ServiceC(c.request(ServiceB.class)))
+                .build();
+
+        container.request(ServiceA.class);
+        container.request(ServiceB.class);
+        container.request(ServiceC.class);
+    }
+
+    @Test
+    void shouldRegisterUsingInterfaceAndImplementationOverloads() {
+        DiContainer container = DiContainer.builder()
+                .transientOf(ServiceInterfaceA.class, ServiceA.class)
+                .transientOf(ServiceA.class)
+                .scopedOf(ServiceInterfaceB.class, ServiceB.class)
+                .scopedOf(ServiceB.class)
+                .singletonOf(ServiceInterfaceC.class, ServiceC.class)
+                .build();
+
+        assertInstanceOf(ServiceA.class, container.request(ServiceInterfaceA.class));
+        assertInstanceOf(ServiceB.class, container.request(ServiceInterfaceB.class));
+        assertInstanceOf(ServiceC.class, container.request(ServiceInterfaceC.class));
     }
 }
